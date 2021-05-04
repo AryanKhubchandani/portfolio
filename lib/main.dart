4fb590e0eb4fca120303f4dfd8c6d8649fce6b52
Aryan <aryan.khubchandani@gmail.com>
2021-05-04 20:13:37 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Portfolio')),
+        body: BodyLayout(),
+      ),
+    );
+  }
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return home(context);
+  }
+}
+
+Widget home(BuildContext context) {
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        title: Text('Hey There!'),
+      ),
+      ListTile(
+        title: Text(
+          'I am Aryan Khubchandani',
+          ),
+        ),
+      ),
+      ListTile(
+        title: Text('Contact Number'),
+      ),
+      ListTile(
+        title: Text('Email ID'),
+      ),
+      ListTile(
+        title: Text('Profiles'),
+      ),
+      ListTile(
+        title: Text('About Me'),
+      ),
+      ListTile(
+        title: Text('Education'),
+      ),
+      ListTile(
+        title: Text('Languages'),
+      ),
+    ],
+  );
+}
